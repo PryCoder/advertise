b80f302de216ad6511a5d0943ed1789789e4ba4a
PryCoder <priyanshugupta007007@gmail.com>
2025-01-22 23:38:32 +0530

    useContext new method by useTheme

--- theme_switcher/tailwind.config.js
+++ theme_switcher/tailwind.config.js
@@ -4,6 +4,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  // darkMode was neccesaay this was static and html for tailwind to understand 
   darkMode: "class",
   theme: {
     extend: {},
