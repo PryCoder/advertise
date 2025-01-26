aae5d936afd73909e86cc0682f8211b5a154d622
PryCoder <priyanshugupta007007@gmail.com>
2025-01-27 03:02:17 +0530

    creating servies of auth and databases by appwrite

---
+++ rec2/jh/src/appwrite/config.js
@@ -0,0 +1,88 @@
+import conf from "../conf/conf"
+
+import {Client, ID, Databases, Storage, Query} from "appwrite"
+
+export class Service{
+       client = new Client()
+       databases;
+       bucket;
+       constructor(){
+        this.client
+          .setEndpoint(conf.appwriteUrl)
+          .setProject(conf.appwriteProjectId)
+          this.databases = new Databases(this.client)
+          this.bucket = new Storage(this.client)
+        }
+
+     async createPost({title, slug, content, image, status, userId}){
+        try {
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionID,
+                slug,
+                {title, content, image, status, userId}
+            )
+
+        } catch (error) {
+            console.log("Appwrite service :: createPost :: error",error)
+        }
+     }
+    
+     async updatePost( slug,{title, content, image, status}){
+        try {
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionID,
+                slug,
+                {title, content, image, status,}
+            )
+
+        } catch (error) {
+            console.log("Appwrite service :: updatePost :: error",error)
+        }
+     }
+
+     async deletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionID,
+                slug
+            )
+        } catch (error) {
+            console.log("Appwrite service :: deletePost :: error",error)
+            return false;
+        }
+     }
+
+   async getPost(slug){
+    try {
+        await this.databases.getDocument(
+            conf.appwriteDatabaseId,
+            conf.appwriteCollectionID,
+            slug
+        )
+    } catch (error) {
+        console.log("Appwrite service :: getPost :: error",error)
+    }
+   }
+
+   async getPosts(queries = [Query.equal("status","active")]){
+     try {
+        return await this.databases.listDocuments(
+            conf.appwriteDatabaseId,
+            conf.appwriteCollectionID,
+            queries,
+        )
+        
+     } catch (error) {
+        console.log("Appwrite service :: getPosts :: error",error)
+     }
+   }
+
+
+}
+
+const service = new Service();
+
+export default service;
